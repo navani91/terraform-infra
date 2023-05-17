c3c9529a3f5a96b782e3674db45b7187a7a52ec4
navaneetha <navani91@gmail.com>
2023-05-17 12:41:08 +0100

    learn-terraform

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,42 +1,53 @@
-variable "instances" {
-  default = {
-    frontend = {
-      name = "frontend"
-      type = "t3.micro"
-    }
-    mongodb = {
-      name = "mongodb"
-      type = "t3.micro"
-    }
- catalogue = {
-   name = "catalogue"
-   type = "t3.micro"
-  }
-  redis = {
-   name = "redis"
-   type = "t3.micro"
-  }
-   user = {
-    name = "user"
-    type = "t3.micro"
- }
- cart = {
-    name = "cart"
-    type = "t3.micro"
-  }
-   mysql = {
-    name = "mysql"
-    type = "t3.micro"
- }
- shipping = {
-   name = "shipping"
-   type = "t3.micro"
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
-  rabbitmq = {
-   name = "rabbitmq"
-   type = "t3.micro"
- }
-  payment = {
-   name = "payment"
-   type = "t3.micro"
- }
+
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [var.sg_id]
+  tags = {
+    Name = var.component
+  }
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "all"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = ""${var.component}-${var.env}-sg""
+  }
+resource "aws_route53_record" "record" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "${var.component}-dev.devopsb71.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+
+variable "component" {}
+variable "instance_type" {}
+variable "sg_id" {}
+variable "env" {
+  default = "dev"
+}
