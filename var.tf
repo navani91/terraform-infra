cbf034d75d7bd3412ab41a9743e8b0493958817e
navaneetha <navani91@gmail.com>
2023-05-17 13:16:28 +0100

    learn-terraform

--- var.tf
+++ var.tf
@@ -27,16 +27,20 @@ variable "instances" {
    mysql = {
     name = "mysql"
     type = "t3.micro"
+    password = "Roboshop@1"
  }
  shipping = {
    name = "shipping"
    type = "t3.micro"
+   password = "Roboshop@1"
 }
   rabbitmq = {
    name = "rabbitmq"
    type = "t3.micro"
+   password = "Roboshop123"
  }
   payment = {
    name = "payment"
    type = "t3.micro"
+   password = "Roboshop123"
  }
